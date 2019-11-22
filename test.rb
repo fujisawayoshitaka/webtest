f4a8277af8cecfce53b73327501b2b3cd95fdd41
yoshitaka fujisawa <1317699@gmail.com>
2019-11-22 04:03:52 +0000

    first

---
+++ test.rb
@@ -0,0 +1,32 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test2.html.erb')
+
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/goya/other.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_other.rb')
+
+server.mount('/goya/false.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_false.rb')
+
+server.start
